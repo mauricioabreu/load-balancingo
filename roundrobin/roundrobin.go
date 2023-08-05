e2b7f6ddc0fc63501d58de15d594907d50f01f29
Maurício Antunes <mauricio.abreua@gmail.com>
2023-08-05 19:14:05 -0300

    feat: Allow servers to be added

--- roundrobin/roundrobin.go
+++ roundrobin/roundrobin.go
@@ -1,8 +1,11 @@
 package roundrobin
 
+import "sync"
+
 type Balancer struct {
 	servers []string
 	index   int
+	m       sync.Mutex
 }
 
 func New(servers []string) *Balancer {
@@ -15,4 +18,15 @@ func (b *Balancer) Next() string {
 	server := b.servers[b.index]
 	b.index = (b.index + 1) % len(b.servers)
 	return server
+}
+
+func (b *Balancer) Add(host string) {
+	b.m.Lock()
+	defer b.m.Unlock()
+
+	b.servers = append(b.servers, host)
+}
+
+func (b *Balancer) Servers() []string {
+	return b.servers
 }
--- roundrobin/roundrobin_test.go
+++ roundrobin/roundrobin_test.go
@@ -7,10 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRoundRobin(t *testing.T) {
+func TestNext(t *testing.T) {
 	b := roundrobin.New([]string{"127.0.0.1", "192.168.0.1", "192.170.0.1"})
 	assert.Equal(t, b.Next(), "127.0.0.1")
 	assert.Equal(t, b.Next(), "192.168.0.1")
 	assert.Equal(t, b.Next(), "192.170.0.1")
 	assert.Equal(t, b.Next(), "127.0.0.1")
+}
+
+func TestAddServer(t *testing.T) {
+	b := roundrobin.New([]string{"127.0.0.1", "192.168.0.1"})
+	assert.Equal(t, len(b.Servers()), 2)
+
+	b.Add("192.170.0.1")
+	assert.Equal(t, len(b.Servers()), 3)
 }
