f629ae560dda1537c2c412318b2c155ba0f278fe
Maurício Antunes <mauricio.abreua@gmail.com>
2023-08-05 22:10:02 -0300

    feat: Implement weighted round robin

--- roundrobin/roundrobin.go
+++ roundrobin/roundrobin.go
@@ -2,31 +2,65 @@ package roundrobin
 
 import "sync"
 
+type server struct {
+	Address       string
+	Weigth        int
+	CurrentWeigth int
+}
+
+func NewServer(address string) *server {
+	return &server{
+		Address:       address,
+		Weigth:        1,
+		CurrentWeigth: 0,
+	}
+}
+
+func (s *server) WithWeigth(weigth int) *server {
+	if weigth <= 0 {
+		panic("weight must be greater than 0")
+	}
+	s.Weigth = weigth
+	return s
+}
+
 type Balancer struct {
-	servers []string
-	index   int
+	servers []*server
 	m       sync.Mutex
 }
 
-func New(servers []string) *Balancer {
+func New(servers ...*server) *Balancer {
 	return &Balancer{
 		servers: servers,
 	}
 }
 
-func (b *Balancer) Next() string {
-	server := b.servers[b.index]
-	b.index = (b.index + 1) % len(b.servers)
-	return server
+func (b *Balancer) Next() *server {
+	maxIndex := -1
+	totalWeight := 0
+
+	for idx, server := range b.servers {
+		b.servers[idx].CurrentWeigth += server.Weigth
+
+		totalWeight += server.Weigth
+
+		if maxIndex == -1 || b.servers[maxIndex].CurrentWeigth < b.servers[idx].CurrentWeigth {
+			maxIndex = idx
+		}
+	}
+
+	b.servers[maxIndex].CurrentWeigth -= totalWeight
+
+	return b.servers[maxIndex]
 }
 
-func (b *Balancer) Add(host string) {
+func (b *Balancer) Add(s *server) {
 	b.m.Lock()
 	defer b.m.Unlock()
 
-	b.servers = append(b.servers, host)
+	b.servers = append(b.servers, s)
 }
 
-func (b *Balancer) Servers() []string {
+func (b *Balancer) Servers() []*server {
 	return b.servers
 }
--- roundrobin/roundrobin_test.go
+++ roundrobin/roundrobin_test.go
@@ -7,18 +7,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNext(t *testing.T) {
-	b := roundrobin.New([]string{"127.0.0.1", "192.168.0.1", "192.170.0.1"})
-	assert.Equal(t, b.Next(), "127.0.0.1")
-	assert.Equal(t, b.Next(), "192.168.0.1")
-	assert.Equal(t, b.Next(), "192.170.0.1")
-	assert.Equal(t, b.Next(), "127.0.0.1")
+func TestSimpleRoundRobin(t *testing.T) {
+	b := roundrobin.New(
+		roundrobin.NewServer("127.0.0.1"),
+		roundrobin.NewServer("192.168.0.1"),
+		roundrobin.NewServer("192.170.0.1"),
+	)
+	assert.Equal(t, b.Next().Address, "127.0.0.1")
+	assert.Equal(t, b.Next().Address, "192.168.0.1")
+	assert.Equal(t, b.Next().Address, "192.170.0.1")
+	assert.Equal(t, b.Next().Address, "127.0.0.1")
+}
+
+func TestWeightedRoundRobin(t *testing.T) {
+	b := roundrobin.New(
+		roundrobin.NewServer("127.0.0.1").WithWeigth(3),
+		roundrobin.NewServer("192.168.0.1").WithWeigth(2),
+		roundrobin.NewServer("192.170.0.1").WithWeigth(1),
+	)
+	assert.Equal(t, b.Next().Address, "127.0.0.1")
+	assert.Equal(t, b.Next().Address, "192.168.0.1")
+	assert.Equal(t, b.Next().Address, "127.0.0.1")
+	assert.Equal(t, b.Next().Address, "192.170.0.1")
 }
 
 func TestAddServer(t *testing.T) {
-	b := roundrobin.New([]string{"127.0.0.1", "192.168.0.1"})
+	b := roundrobin.New(
+		roundrobin.NewServer("127.0.0.1"),
+		roundrobin.NewServer("192.168.0.1"),
+	)
 	assert.Equal(t, len(b.Servers()), 2)
 
-	b.Add("192.170.0.1")
+	b.Add(roundrobin.NewServer("192.170.0.1"))
 	assert.Equal(t, len(b.Servers()), 3)
 }
