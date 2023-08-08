7c04fc2d9660919686ff2cc02b0ee49117d260e3
Maurício Antunes <mauricio.abreua@gmail.com>
2023-08-08 01:02:50 -0300

    chore: Document NewProxy function

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -11,6 +11,9 @@ type Balancer interface {
 	NextAddress() string
 }
 
+// NewProxy implements a reverse proxy that will load balance requests.
+// It receives a Balancer interface that will be used to get the next address
+// working as a balancing strategy.
 func NewProxy(b Balancer) *httputil.ReverseProxy {
 	p := &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
